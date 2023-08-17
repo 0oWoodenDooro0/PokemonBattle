dec7f2176b0bc860817d5e41bf9f4a28a0189716
0oWoodenDooro0 <vincent031525@gmail.com>
2023-08-17 23:31:17 +0800

    新增Move metadata

--- move.py
+++ move.py
@@ -5,15 +5,28 @@ class Move:
     def __init__(self, move_id: int):
         self.id = move_id
         self.data = util.fetch_json('move', str(move_id))
-        self.name = self.data['name'].capitalize()
+
         self.accuracy = self.data['accuracy']
+        self.damage_class = util.url_to_id(self.data['damage_class']['url'], 'move-damage-class')
         self.effect_chance = self.data['effect_chance']
+        self.meta = self.data['meta']
+        self.ailment = util.url_to_id(self.meta['aliment']['url'], 'move-ailment')
+        self.ailment_chance = self.meta['ailment_chance']
+        self.category = self.meta['category']
+        self.crit_rate = self.meta['crit_rate']
+        self.drain = self.meta['drain']
+        self.flinch_chance = self.meta['flinch_chance']
+        self.healing = self.meta['healing']
+        self.max_hits = self.meta['max_hits']
+        self.max_turns = self.meta['max_turns']
+        self.min_hits = self.meta['min_hits']
+        self.min_turns = self.meta['min_turns']
+        self.stat_chance = self.meta['stat_chance']
+        self.name = self.data['name'].capitalize()
+        self.power = self.data['power']
         self.pp = self.data['pp']
         self.max_pp = self.data['pp']
         self.priority = self.data['priority']
-        self.power = self.data['power']
-        self.crit_rate = self.data['meta']['crit_rate']
         self.stat_changes = [{'stat': util.url_to_id(x['stat']['url'], 'stat'), 'change': x['change']} for x in self.data['stat_changes']]
-        self.type = util.url_to_id(self.data['type']['url'], 'type')
-        self.damage_class = util.url_to_id(self.data['damage_class']['url'], 'move-damage-class')
         self.move_target = util.url_to_id(self.data['target']['url'], 'move-target')
+        self.type = util.url_to_id(self.data['type']['url'], 'type')
